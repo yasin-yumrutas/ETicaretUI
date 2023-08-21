5b7ed5fd53661be510b60955b3be7e2d71b92379
yasin-yumrutas <yasinyumrutas0@gmail.com>
2023-08-21 21:25:04 +0300

    CLI_UI

---
+++ src/app/admin/components/order/order.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrderComponent } from './order.component';
+import {RouterModule} from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    OrderComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: "", component: OrderComponent }
+    ])
+  ]
+})
+export class OrderModule { }
